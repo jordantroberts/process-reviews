f74cbcca3abb809ddf499a58ee88d76805f9a3ed
Jordan Roberts <jordanroberts@talk21.com>
2019-06-22 13:01:07 +0100

    Added time and date to the response

--- Echo/lib/echo.rb
+++ Echo/lib/echo.rb
@@ -1,8 +1,9 @@
+require 'Date'
 class Echo
 
   def echo(phrase)
     puts "Please enter a word"
     phrase = gets.chomp!
-    return phrase
+    return Date.today.strftime("%d/%m/%Y") + ' | ' + Time.now.strftime('%H:%M') + ' | ' + 'You said: ' + phrase
   end
 end
--- Echo/spec/echo_spec.rb
+++ Echo/spec/echo_spec.rb
@@ -2,9 +2,12 @@ require 'echo'
 
 describe Echo do
 
+
   describe '#echo' do
     it 'repeats the word back to you' do
-      expect(subject.echo("Hello")).to eq "Hello"
+      allow(Date).to receive(:today).and_return(Date.new(2019, 6, 20))
+      allow(Time).to receive(:now).and_return(Time.new(2019, 6, 20, 9, 0))
+      expect(subject.echo("Hello")).to eq '20/06/2019 | 09:00 | You said: Hello'
     end
   end
 
